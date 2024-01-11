17421692d0740d2abbff2102bd9b0d856c52ea9a
Kartik Gupta <112673521+kartikgupta09@users.noreply.github.com>
2024-01-12 00:35:25 +0530

    Update config.py

--- GoSwift_project/config.py
+++ GoSwift_project/config.py
@@ -1,10 +1,11 @@
-columns_to_keep = [
-    "Tracking number",
-    "Payment type (Prepaid/COD)",
-    "Pickup Date Time",
-    "Delivery Date Time",
-    "Out for Delivery datetime(s)",
-    "Shipment weight",
-    "Pickup Pincode, City, State",
-    "Drop Pincode, City, State"
-]
+columns = ["Tracking number", 
+           "Payment type (Prepaid/COD)",
+           "Pickup Date Time", 
+           "Delivery Date Time", 
+           "Days Taken for Delivery",
+           "Out for Delivery datetime(s)", 
+           "Shipment weight", 
+           "Pickup Pincode, City, State", 
+           "Drop Pincode, City, State",
+           "Number of Delivery Attempts Needed"
+           ]
